fc14ba357b96e8b856ca7fec938336722d3dc8a1
hussain658 <bsdsf24m037@pucit.edu.pk>
2024-12-31 08:43:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "Atif Hussain";
+let passion ="Politics";
+let activity = "cricket";
+let education = "BS";
+let profession="Data Scientist";
+let dream="Honorable Prime Minister of Islamic Republic of Pakistan";
+let I_want_what="proseperous pakistan at every race";
+console.log("Name: " name);
+console.log("passion's Name: " passion);
+console.log(" activity: "  activity);
+console.log("Education:" education);
+console.log(" profession:"  profession);
+console.log(" dream:" dream);
+console.log(" want:"  I_want_what);
+
